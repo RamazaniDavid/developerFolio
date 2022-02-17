13d3c680b115660da0a0df46b2cf69eba9e4c455
ProDeveloper <ramazani.david@gmail.com>
2022-02-17 17:55:07 +0330

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Hi all, I'm David 👋
-A passionate Full Stack Software Developer 🚀 with many years of experience designing and creating large-scale systems such as financial systems and MIS with .Net Core / SQL server / JavaScript / Angular / AngularJS and some other cool libraries and frameworks.
---
+++ precache-manifest.df59133e6deffedd0cb88d88ec2c35ae.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c550ca4bfc7b6c96905e0e89ea384e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b02fa01f9e2e464a848c",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8656da5fe43c6cd2d849",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "73c955c83998a25d3969",
+    "url": "/static/css/main.43488906.chunk.css"
+  },
+  {
+    "revision": "6099da15d7ef139536ef",
+    "url": "/static/js/2.3b89ba20.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.3b89ba20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02fa01f9e2e464a848c",
+    "url": "/static/js/3.6d3a6ac5.chunk.js"
+  },
+  {
+    "revision": "8656da5fe43c6cd2d849",
+    "url": "/static/js/4.0eb2741e.chunk.js"
+  },
+  {
+    "revision": "73c955c83998a25d3969",
+    "url": "/static/js/main.7037f769.chunk.js"
+  },
+  {
+    "revision": "2f53130181c1bd58b21d",
+    "url": "/static/js/runtime-main.b139d0f2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "997e0ae1919e421265f281d2f359074a",
+    "url": "/static/media/erec.997e0ae1.png"
+  },
+  {
+    "revision": "7d8edeb6190b47883f94ac9198453af4",
+    "url": "/static/media/faraandish.7d8edeb6.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "264d74a57a6cf2523d78e6dce71bbea6",
+    "url": "/static/media/pnu.264d74a5.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "6db75e60d50ec2569740c9a5ce70cb52",
+    "url": "/static/media/sysnatech.6db75e60.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
